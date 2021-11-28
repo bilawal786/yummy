6898c0fb979a71c8b488ed6830d2106040d8f369
sonu <bilawal.jabbar.786@gmail.com>
2021-11-29 00:12:07 +0500

    new

--- resources/views/frontend/shopMap.blade.php
+++ resources/views/frontend/shopMap.blade.php
@@ -2,6 +2,6 @@
 @section('main-content')
     <div class="page-content-wrapper">
         <iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAeKxMwTMJzHH2AR1xt7OLWIWFMIzm-JLM&libraries
-&query={{$lat}},{{$lan}}" width="100%" style="border:0; height: 100vh" allowfullscreen="" loading="lazy"></iframe>
+&q={{$lat}},{{$lan}}" width="100%" style="border:0; height: 100vh" allowfullscreen="" loading="lazy"></iframe>
     </div>
 @endsection
