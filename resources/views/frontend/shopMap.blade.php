dd3ed87fd1ff9f341415d83742ccce8b20d8e2e1
sonu <bilawal.jabbar.786@gmail.com>
2021-11-29 00:11:30 +0500

    new

--- resources/views/frontend/shopMap.blade.php
+++ resources/views/frontend/shopMap.blade.php
@@ -1,6 +1,7 @@
 @extends('frontend.layouts.mobile')
 @section('main-content')
     <div class="page-content-wrapper">
-        <iframe src="https://www.google.com/maps/search/?api=1&query={{$lat}},{{$lan}}" width="100%" style="border:0; height: 100vh" allowfullscreen="" loading="lazy"></iframe>
+        <iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAeKxMwTMJzHH2AR1xt7OLWIWFMIzm-JLM&libraries
+&query={{$lat}},{{$lan}}" width="100%" style="border:0; height: 100vh" allowfullscreen="" loading="lazy"></iframe>
     </div>
 @endsection
