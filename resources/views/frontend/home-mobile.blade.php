fc51b935453d5920f61fb00600d9da41a8ca279f
sonu <bilawal.jabbar.786@gmail.com>
2021-11-28 19:27:34 +0500

    new

--- resources/views/frontend/home-mobile.blade.php
+++ resources/views/frontend/home-mobile.blade.php
@@ -196,6 +196,9 @@
 
 @section('footer-js')
     <script src="https://www.gstatic.com/firebasejs/7.23.0/firebase.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/8.3.2/firebase-app.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/8.3.2/firebase-messaging.js"></script>
+
     <script>
 
         var firebaseConfig = {
