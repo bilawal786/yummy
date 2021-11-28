f4ec44c36ccac1164b8e6f3db9850d33caf45d45
sonu <bilawal.jabbar.786@gmail.com>
2021-11-29 00:14:37 +0500

    new

--- resources/views/frontend/shopMap.blade.php
+++ resources/views/frontend/shopMap.blade.php
@@ -1,7 +1,8 @@
 @extends('frontend.layouts.mobile')
 @section('main-content')
     <div class="page-content-wrapper">
-        <iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAeKxMwTMJzHH2AR1xt7OLWIWFMIzm-JLM&libraries
-&q={{$lat}},{{$lan}}" width="100%" style="border:0; height: 100vh" allowfullscreen="" loading="lazy"></iframe>
+        <iframe src="https://maps.google.com/maps?q={{$lat}},{{$lan}}&hl=es&z=14&amp;output=embed" width="100%" style="border:0; height: 100vh" allowfullscreen="" loading="lazy"></iframe>
+<!--        <iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAeKxMwTMJzHH2AR1xt7OLWIWFMIzm-JLM&libraries
+&q={{$lat}},{{$lan}}" width="100%" style="border:0; height: 100vh" allowfullscreen="" loading="lazy"></iframe>-->
     </div>
 @endsection
