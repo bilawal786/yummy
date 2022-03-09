6d3437e52a3a8f857313dcd4342674d3e67d3da2
Sonu <bilawal.jabbar.786@gmail.com>
2022-03-09 16:18:21 +0500

    shopadmins

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -10,36 +10,36 @@ use App\Models\OrderLineItem;
 use Carbon\Carbon;
 class CronController extends Controller
 {
-    public function product_update()
-    {
-      $mytime = Carbon::now();
-      $heure = $mytime->format('H:i:s');
-      $products = ShopProduct::where('hdispob', '<=', $heure)->get();
-      foreach($products as $row){
-        $product = ShopProduct::find($row['id']);
-        $product->quantity = 0;
-        $product->save();
-      }
-      return $products;
-    }
-    public function order_update()
-    {
-      $mytime = Carbon::now();
-      $heure = $mytime->format('H:i:s');
-      $orders_line = OrderLineItem::with('shop_product')->with('order')->get();
-
-      foreach($orders_line as $row){
-        foreach($row->shop_product as $dispo){
-
-          if($row->order->status == '17'){
-            if($dispo >= $heure){
-              $orderl = OrderLineItem::find($row['id']);
-              $orderl->order()->update(['status' => '10']);
-            }
-          }
-        }
-      }
-    }
+//    public function product_update()
+//    {
+//      $mytime = Carbon::now();
+//      $heure = $mytime->format('H:i:s');
+//      $products = ShopProduct::where('hdispob', '<=', $heure)->get();
+//      foreach($products as $row){
+//        $product = ShopProduct::find($row['id']);
+//        $product->quantity = 0;
+//        $product->save();
+//      }
+//      return $products;
+//    }
+//    public function order_update()
+//    {
+//      $mytime = Carbon::now();
+//      $heure = $mytime->format('H:i:s');
+//      $orders_line = OrderLineItem::with('shop_product')->with('order')->get();
+//
+//      foreach($orders_line as $row){
+//        foreach($row->shop_product as $dispo){
+//
+//          if($row->order->status == '17'){
+//            if($dispo >= $heure){
+//              $orderl = OrderLineItem::find($row['id']);
+//              $orderl->order()->update(['status' => '10']);
+//            }
+//          }
+//        }
+//      }
+//    }
     public function order_status(){
         $orders = Order::where('status', 17)->get();
         foreach ($orders as $order){
