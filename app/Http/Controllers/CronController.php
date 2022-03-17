40423cf2213fe6fee4d61d0cc1fa9ad95ed180ae
Sonu <bilawal.jabbar.786@gmail.com>
2022-03-17 11:51:10 +0500

    $cate

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -22,24 +22,24 @@ class CronController extends Controller
       }
       return $products;
     }
-    public function order_update()
-    {
-      $mytime = Carbon::now();
-      $heure = $mytime->format('H:i:s');
-      $orders_line = OrderLineItem::with('shop_product')->with('order')->get();
-
-      foreach($orders_line as $row){
-        foreach($row->shop_product as $dispo){
-
-          if($row->order->status == '17'){
-            if($dispo >= $heure){
-              $orderl = OrderLineItem::find($row['id']);
-              $orderl->order()->update(['status' => '10']);
-            }
-          }
-        }
-      }
-    }
+//    public function order_update()
+//    {
+//      $mytime = Carbon::now();
+//      $heure = $mytime->format('H:i:s');
+//      $orders_line = OrderLineItem::with('shop_product')->with('order')->get();
+//
+//      foreach($orders_line as $row){
+//        foreach($row->shop_product as $dispo){
+//
+//          if($row->order->status == '17'){
+//            if($dispo >= $heure){
+//              $orderl = OrderLineItem::find($row['id']);
+//              $orderl->order()->update(['status' => '10']);
+//            }
+//          }
+//        }
+//      }
+//    }
     public function order_status(){
         $orders = Order::where('status', 17)->get();
         foreach ($orders as $order){
