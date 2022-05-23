ea8820df2fb83bc1b88a3818260bd6d56c380a51
usman299 <usman@codingcrust.com>
2022-05-23 05:02:02 -0700

    klnjhui

--- resources/views/admin/layouts/components/navigation.blade.php
+++ resources/views/admin/layouts/components/navigation.blade.php
@@ -3,7 +3,7 @@
        background-color: green!important;
     }
 </style>
-
+{{dd(config('app.url'))}}
     <div class="navbar-bg {{ url('https://demo.yummybox.fr/') ? 'active':'' }}" ></div>
 
 <nav class="navbar navbar-expand-lg main-navbar">
