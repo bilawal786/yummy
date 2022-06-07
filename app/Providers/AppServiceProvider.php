cc0034e605cd138d2b5a978b1003075eeb484ad4
usman299 <usman@codingcrust.com>
2022-06-07 04:57:34 -0700

    sa',sw. aa

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -37,7 +37,7 @@ class AppServiceProvider extends ServiceProvider
         $geo = GeoIP::getLocation($ip);
         //Get visitors country name
         $country = $geo['country'];
-dd($geo);
+
         //Prepared language based on country name
         //Add as many as you want
         $languages = [
@@ -50,6 +50,7 @@ dd($geo);
             'Austria' => 'de',
             'Luxembourg' => 'de',
             'Guadeloupe' => 'gp',
+            'Pakistan' => 'pk',
         ];
 
         if(!isset($cookie) && !empty($cookie)) {
